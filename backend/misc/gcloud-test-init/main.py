186e6998d4ea09f6c64009a57e1a7c44c3563515
pietro <pietro.g.moroni@gmail.com>
2024-03-29 16:57:56 +0100

    Update main.py

--- backend/misc/gcloud-test-init/main.py
+++ backend/misc/gcloud-test-init/main.py
@@ -6,6 +6,7 @@ from flask import Flask, request
 load_dotenv()
 
 app = Flask(__name__)
+speechClient = speech.SpeechClient()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -30,31 +31,12 @@ def hello_world():
 
 
 def run_quickstart(audio_stream: bytes) -> speech.RecognizeResponse:
-
-    client = speech.SpeechClient()
-
-    # The name of the audio file to transcribe
-    # gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
-    # uri = "/content/drive/MyDrive/TeachMe/audio_samples/harvard.wav"
-
-    # A RecognitionAudio object contains audio data derived from the resource passed as argument
-    # audio = speech.RecognitionAudio(uri=gcs_uri)
     audio = speech.RecognitionAudio(content=audio_stream)
-    # with open(uri, 'rb') as f:
-    #   audio = speech.RecognitionAudio(content=f.read())
-
-    # A RecognitionConfig object is needed to provide information to the recognizer about how to process the request
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         audio_channel_count=2,  # 1 channel for raw, 2 channels for wav
         sample_rate_hertz=44100,  # 16000 for raw, 44100 for wav
         language_code="en-US",
     )
-
-    # Detects speech in the audio file
-    response = client.recognize(config=config, audio=audio)
-
-    for result in response.results:
-        print(f"Transcript: {result.alternatives[0].transcript}")
-
+    response = speechClient.recognize(config=config, audio=audio)
     return response
