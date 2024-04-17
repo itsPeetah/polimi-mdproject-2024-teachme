b1f1c841642afd0caa1accfe5ccf9d7521aefb00
Paolo Pertino <pertino.paolo@gmail.com>
2024-04-17 11:49:06 +0200

    a

--- backend/main.py
+++ backend/main.py
@@ -159,41 +159,31 @@ def get_chatbot_answer(prompt: str) -> str:
     response = chatbot.send_message(prompt).content
     return response
 
-def load_audio(file_path):
-    with open(file_path,'rb') as audio_file:
-        # Getting the audio file parameters
-        # Read the header to get audio file information
-        
-        header = audio_file.read(44) # In WAV files, first 44 bytes are reserved for the header
-        print(f"The header is {header}")
-        
-        if header[:4] != b'RIFF' or header[8:12] != b'WAVE' or header[12:16] != b'fmt ':
-            raise ValueError("Invalid WAV file")
-            
-        # Extract relevant information from the header
-        header_chunk_id = struct.unpack('4s', header[0:4])[0]
-        header_chunk_size = struct.unpack('<I', header[4:8])[0]
-        header_chunk_format = struct.unpack('4s', header[8:12])[0]
-        format_chunk_id = struct.unpack('4s', header[12:16])[0]
-        format_chunk_size = struct.unpack('<I', header[16:20])[0]
-        format_code = struct.unpack('<H', header[20:22])[0]
-        channels = struct.unpack('<H', header[22:24])[0]
-        sample_rate = struct.unpack('<I', header[24:28])[0]
-        byte_rate = struct.unpack('<I', header[28:32])[0]
-        block_align = struct.unpack('<H', header[32:34])[0]
-        sample_width = struct.unpack('<H', header[34:36])[0]
-        data_chunk_id = struct.unpack('4s', header[36:40])[0]
-        data_chunk_size = struct.unpack('<I', header[40:44])[0]
-        
-        # Read the data from the file
-        audio_file.seek(44);
-        data = audio_file.read(data_chunk_size)
-#         data = audio_file.read()
+def load_audio(audio_file):
+    # Getting the audio file parameters
+    # Read the header to get audio file information
     
-    # Converting the raw binary data to a list of integers : 
-    data_array = np.frombuffer(data, dtype=np.int32)
-    # Convert to float32
-    data_array = data_array.astype(np.float32)
+    header = audio_file.read(44) # In WAV files, first 44 bytes are reserved for the header
+    print(f"The header is {header}")
+    
+    if header[:4] != b'RIFF' or header[8:12] != b'WAVE' or header[12:16] != b'fmt ':
+        raise ValueError("Invalid WAV file")
+        
+    # Extract relevant information from the header
+    header_chunk_id = struct.unpack('4s', header[0:4])[0]
+    header_chunk_size = struct.unpack('<I', header[4:8])[0]
+    header_chunk_format = struct.unpack('4s', header[8:12])[0]
+    format_chunk_id = struct.unpack('4s', header[12:16])[0]
+    format_chunk_size = struct.unpack('<I', header[16:20])[0]
+    format_code = struct.unpack('<H', header[20:22])[0]
+    channels = struct.unpack('<H', header[22:24])[0]
+    sample_rate = struct.unpack('<I', header[24:28])[0]
+    byte_rate = struct.unpack('<I', header[28:32])[0]
+    block_align = struct.unpack('<H', header[32:34])[0]
+    sample_width = struct.unpack('<H', header[34:36])[0]
+    data_chunk_id = struct.unpack('4s', header[36:40])[0]
+    data_chunk_size = struct.unpack('<I', header[40:44])[0]
+    
     
     return_dict = {
         'header_chunk_id': header_chunk_id,
@@ -201,7 +191,6 @@ def load_audio(file_path):
         'header_chunk_format': header_chunk_format,
         'format_chunk_id': format_chunk_id,
         'format_chunk_size': format_chunk_size,
-        'data_array': data_array,
         'format_code': format_code,
         'channels': channels,
         'sample_width': sample_width,
