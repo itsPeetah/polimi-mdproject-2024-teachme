bf3a738f9313420bf6f18485e9f7cdf1c91bb66c
Paolo Pertino <pertino.paolo@gmail.com>
2024-04-17 11:43:41 +0200

    a

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 import time
+import struct
 
 from os import system, getenv
+
+import numpy as np
 
 from lib.llm import ConversationalChatBot
 from lib.database import MongoDBConnector
@@ -128,11 +131,12 @@ def on_audio_data(data: bytes):
 
 
 def get_user_transcript(audio_stream: bytes) -> speech.RecognizeResponse:
+    audio_headers = load_audio(audio_stream)
     audio = speech.RecognitionAudio(content=audio_stream)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        # audio_channel_count=2,  # 1 channel for raw, 2 channels for wav
-        # sample_rate_hertz=44100,  # 16000 for raw, 44100 for wav
+        audio_channel_count=audio_headers.get('channels'),  # 1 channel for raw, 2 channels for wav
+        sample_rate_hertz=audio_headers.get('sample_rate'),  # 16000 for raw, 44100 for wav
         language_code="en-US",
     )
     response = speechClient.recognize(config=config, audio=audio)
@@ -154,6 +158,61 @@ def get_chatbot_answer(prompt: str) -> str:
     )
     response = chatbot.send_message(prompt).content
     return response
+
+def load_audio(file_path):
+    with open(file_path,'rb') as audio_file:
+        # Getting the audio file parameters
+        # Read the header to get audio file information
+        
+        header = audio_file.read(44) # In WAV files, first 44 bytes are reserved for the header
+        print(f"The header is {header}")
+        
+        if header[:4] != b'RIFF' or header[8:12] != b'WAVE' or header[12:16] != b'fmt ':
+            raise ValueError("Invalid WAV file")
+            
+        # Extract relevant information from the header
+        header_chunk_id = struct.unpack('4s', header[0:4])[0]
+        header_chunk_size = struct.unpack('<I', header[4:8])[0]
+        header_chunk_format = struct.unpack('4s', header[8:12])[0]
+        format_chunk_id = struct.unpack('4s', header[12:16])[0]
+        format_chunk_size = struct.unpack('<I', header[16:20])[0]
+        format_code = struct.unpack('<H', header[20:22])[0]
+        channels = struct.unpack('<H', header[22:24])[0]
+        sample_rate = struct.unpack('<I', header[24:28])[0]
+        byte_rate = struct.unpack('<I', header[28:32])[0]
+        block_align = struct.unpack('<H', header[32:34])[0]
+        sample_width = struct.unpack('<H', header[34:36])[0]
+        data_chunk_id = struct.unpack('4s', header[36:40])[0]
+        data_chunk_size = struct.unpack('<I', header[40:44])[0]
+        
+        # Read the data from the file
+        audio_file.seek(44);
+        data = audio_file.read(data_chunk_size)
+#         data = audio_file.read()
+    
+    # Converting the raw binary data to a list of integers : 
+    data_array = np.frombuffer(data, dtype=np.int32)
+    # Convert to float32
+    data_array = data_array.astype(np.float32)
+    
+    return_dict = {
+        'header_chunk_id': header_chunk_id,
+        'header_chunk_size': header_chunk_size,
+        'header_chunk_format': header_chunk_format,
+        'format_chunk_id': format_chunk_id,
+        'format_chunk_size': format_chunk_size,
+        'data_array': data_array,
+        'format_code': format_code,
+        'channels': channels,
+        'sample_width': sample_width,
+        'sample_rate': sample_rate,
+        'byte_rate': byte_rate,
+        'block_align': block_align,
+        'data_chunk_id': data_chunk_id,
+        'data_chunk_size': data_chunk_size
+    }
+    
+    return return_dict
 
 
 def run_quickstart(audio_stream: bytes) -> speech.RecognizeResponse:
