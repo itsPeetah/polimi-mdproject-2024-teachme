3619c9004b72f52b8a4f25b4fa04949414249c00
pietro <pietro.g.moroni@gmail.com>
2024-05-07 17:29:25 +0200

    Import refactoring and removed unused code

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,23 @@
-import io
-import struct
+# stdlib imports
 from os import getenv, system
 
+# dependency imports
 from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
-from flask import (
-    Flask,
-    Response,
-    jsonify,
-    make_response,
-    request,
-)
+from flask import Flask
 from flask_cors import CORS
-from lib.routes.conversationRoutes import register_conversation_routes
-from lib.routes.userRoutes import register_user_routes
-from lib.routes.logRoutes import register_log_routes
-from lib.routes.authRoutes import register_auth_routes
+
+# custom imports
+from lib.routes import *
 from lib.auth import AuthenticationService
 from lib.database import MongoDBConnector
-from lib.llm import ConversationalChatBot, test_chatbot
-from lib.log import Logger, LogType, Log
+from lib.log import Logger
+
+# execution
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app, origins="*")
-EL_client = ElevenLabs(api_key=getenv("ELEVENLABS_API_KEY"))
 app_db_connector = MongoDBConnector(
     getenv("MONGODB_URI")
 )  # TEST if this works otherwise declare global client?
@@ -39,11 +31,9 @@ register_user_routes(app, db, logger)
 register_conversation_routes(app, db)
 
 if __name__ == "__main__":
-
-    # user_auth.make_friends()
-    # logger.log(Log(LogType.INFO, "Starting Flask app"))
-    # conv_dict = user_auth.create_conversation()
     system("python3 -m flask --app main run --debug")
+
+    # from lib.llm import test_chatbot
     # test_chatbot(
     #     api_key=getenv("OPENAI_API_KEY"),
     #     conversation_id="6639ea3410a08c3689597981",
