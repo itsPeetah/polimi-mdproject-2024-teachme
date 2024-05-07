975fea731e6226546d295b7557ca37595a526783
pietro <pietro.g.moroni@gmail.com>
2024-05-07 17:07:14 +0200

    Removed deprecated functions

--- backend/main.py
+++ backend/main.py
@@ -31,21 +31,10 @@ db = app_db_connector.connect("teachme_main")
 user_auth = AuthenticationService(db)
 logger = Logger(db)
 
-audio_buffer_handlers = {}
-
 register_auth_routes(app, user_auth)
 register_log_routes(app, db)
 
 # ROUTES
-
-
-@app.route("/text-to-speech", methods=["GET"])
-def text_to_speech():
-    text = request.args.get("text")
-    audio_tts = EL_client.generate(text=text)
-    response = Response(audio_tts, mimetype="audio/wav")
-    # response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
 
 
 @app.route("/create-friendship", methods=["POST"])
@@ -111,71 +100,12 @@ def get_friends(user_email: str):
     return jsonify(user_friends)
 
 
-def get_chatbot_answer(prompt: str) -> str:
-    chatbot = ConversationalChatBot(
-        api_key=getenv("OPENAI_API_KEY"),
-        conversation_id=1,
-        db_connector=app_db_connector,
-        logger=logger,
-    )
-    response = chatbot.send_message(prompt).content
-    return response
-
-
-def load_audio(
-    audio_file,
-):  # TODO: DEPRECATED TO ADJUST WITH NEW LOGIC (useless, could be removed)
-    # Getting the audio file parameters
-    # Read the header to get audio file information
-    audio_file = io.BytesIO(audio_file)
-    header = audio_file.read(
-        44
-    )  # In WAV files, first 44 bytes are reserved for the header
-    print(f"The header is {header}")
-
-    if header[:4] != b"RIFF" or header[8:12] != b"WAVE" or header[12:16] != b"fmt ":
-        raise ValueError("Invalid WAV file")
-
-    # Extract relevant information from the header
-    header_chunk_id = struct.unpack("4s", header[0:4])[0]
-    header_chunk_size = struct.unpack("<I", header[4:8])[0]
-    header_chunk_format = struct.unpack("4s", header[8:12])[0]
-    format_chunk_id = struct.unpack("4s", header[12:16])[0]
-    format_chunk_size = struct.unpack("<I", header[16:20])[0]
-    format_code = struct.unpack("<H", header[20:22])[0]
-    channels = struct.unpack("<H", header[22:24])[0]
-    sample_rate = struct.unpack("<I", header[24:28])[0]
-    byte_rate = struct.unpack("<I", header[28:32])[0]
-    block_align = struct.unpack("<H", header[32:34])[0]
-    sample_width = struct.unpack("<H", header[34:36])[0]
-    data_chunk_id = struct.unpack("4s", header[36:40])[0]
-    data_chunk_size = struct.unpack("<I", header[40:44])[0]
-
-    return_dict = {
-        "header_chunk_id": header_chunk_id,
-        "header_chunk_size": header_chunk_size,
-        "header_chunk_format": header_chunk_format,
-        "format_chunk_id": format_chunk_id,
-        "format_chunk_size": format_chunk_size,
-        "format_code": format_code,
-        "channels": channels,
-        "sample_width": sample_width,
-        "sample_rate": sample_rate,
-        "byte_rate": byte_rate,
-        "block_align": block_align,
-        "data_chunk_id": data_chunk_id,
-        "data_chunk_size": data_chunk_size,
-    }
-
-    return return_dict
-
-
 if __name__ == "__main__":
 
     # user_auth.make_friends()
     # logger.log(Log(LogType.INFO, "Starting Flask app"))
     # conv_dict = user_auth.create_conversation()
-    system("python3 -m flask --app main run --host=0.0.0.0 --port=5000 --debug")
+    system("python3 -m flask --app main run --debug")
     # test_chatbot(
     #     api_key=getenv("OPENAI_API_KEY"),
     #     conversation_id="6639ea3410a08c3689597981",
